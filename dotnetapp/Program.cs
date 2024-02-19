95cde7276e6ee986b0202782d3511cb838e871f2
neojarvis <techadmin@iamneo.ai>
2024-02-19 04:26:26 +0000

    Auto-save-commit-19/02/2024--09:56:24

--- dotnetapp/Program.cs
+++ dotnetapp/Program.cs
@@ -1,15 +1,63 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Text;
+using System.Threading.Tasks;
+using dotnetapp.Models;
+using dotnetapp.Repository;
+using dotnetapp.Service;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowOrigin",
+        builder =>
+        {
+            builder.WithOrigins("http://localhost:8080")
+                   .AllowAnyHeader()
+                   .AllowAnyMethod();
+        });
+});
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DevConnection")));
 
+// Add authentication services
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = builder.Configuration["JWT:Issuer"],
+            ValidAudience = builder.Configuration["JWT:Audience"],
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["JWT:Key"]))
+        };
+    });
+
+// Register services and repositories
+builder.Services.AddScoped<UserService, UserServiceImpl>(); 
+builder.Services.AddScoped<UserRepo>();
+// builder.Services.AddScoped<ResortService, ResortServiceImpl>();
+// builder.Services.AddScoped<ResortRepo>();
+// builder.Services.AddScoped<ReviewService, ReviewServiceImpl>();
+// builder.Services.AddScoped<ReviewRepo>();
+// builder.Services.AddScoped<BookingService, BookingServiceImpl>();
+// builder.Services.AddScoped<IBookingRepo, BookingRepo>();
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -17,9 +65,13 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.UseRouting();
 
+// Apply CORS policy
+app.UseCors("AllowOrigin");
+
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
-
 app.Run();
