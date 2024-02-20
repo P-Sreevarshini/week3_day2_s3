56db45f6be31b56f0a6a63e7b965d513a9091f64
neojarvis <techadmin@iamneo.ai>
2024-02-20 11:46:09 +0000

    Auto-save-commit-20/02/2024--17:16:08

--- angularapp/src/apiconfig.ts
+++ angularapp/src/apiconfig.ts
@@ -1,1 +1,1 @@
-export const apiUrl = 'https://8080-fcebccfce309511144aaeaaecbccfdbone.premiumproject.examly.io'; // Replace with your Spring Boot backend URL
+export const apiUrl = 'https://8080-dfbbeddfccdbcfacbdcbaeadbebabcdebdca.premiumproject.examly.io'; // Replace with your Spring Boot backend URL
