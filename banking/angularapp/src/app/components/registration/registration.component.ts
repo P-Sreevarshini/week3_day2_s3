626ef69017516887edde602cbf3e3e8cda86f4fb
neojarvis <techadmin@iamneo.ai>
2024-02-28 11:23:27 +0000

    Auto-save-commit-28/02/2024--16:53:23

--- banking/angularapp/src/app/components/registration/registration.component.ts
+++ banking/angularapp/src/app/components/registration/registration.component.ts
@@ -1,1 +1,66 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
+const UserRoles = {
+  Admin: 'Admin',
+  User: 'Customer'
+};
+@Component({
+  selector: 'app-registration',
+  templateUrl: './registration.component.html',
+  styleUrls: ['./registration.component.css']
+})
+export class RegistrationComponent {
+  userName: string = "";
+  password: string = "";
+  confirmPassword: string = "";
+  mobileNumber: string = "";
+  role: string = ""; // Use string constants instead of hardcoding role values
+  email: string;
+  passwordMismatch: boolean = false;
+  emailExistsError: boolean = false;
 
+  constructor(private authService: AuthService, private router: Router) { }
+
+  register(): void {
+    if (this.password !== this.confirmPassword) {
+      this.passwordMismatch = true;
+      return;
+    }
+
+    this.passwordMismatch = false;
+
+    if (!this.isPasswordComplex(this.password)) {
+      return; // Password complexity check failed
+    }
+
+    this.authService.register(this.userName, this.password, this.role, this.email, this.mobileNumber).subscribe(
+      (user) => {
+        console.log(user);
+
+        // Use role constants defined in C# code
+        if (user == true && this.role === UserRoles.Admin) {
+          alert('Registration Successful');
+          this.router.navigate(['/api/login']);
+        } else if (user == true && this.role === UserRoles.User) {
+          alert('Registration Successful');
+          this.router.navigate(['/api/login']);
+        } else {
+          alert('Registration failed. User with that Email already exists or an error occurred. Please try again.');
+        }
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
+
+  isPasswordComplex(password: string): boolean {
+    const hasUppercase = /[A-Z]/.test(password);
+    const hasLowercase = /[a-z]/.test(password);
+    const hasDigit = /\d/.test(password);
+    const hasSpecialChar = /[!@#$%^&*()_+{}\[\]:;<>,.?~\-]/.test(password);
+
+    return hasUppercase && hasLowercase && hasDigit && hasSpecialChar;
+  }
+}
---
+++ banking/angularapp/src/app/models/userRole.model.ts
@@ -0,0 +1,6 @@
+export class UserRoles 
+{
+    public static readonly Admin: string = 'Admin';
+    public static readonly User: string = 'Customer';
+}
+  
