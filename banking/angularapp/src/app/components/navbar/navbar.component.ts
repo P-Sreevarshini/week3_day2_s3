3fd2137d7abc9a8df808e51370e98f5664c052ae
neojarvis <techadmin@iamneo.ai>
2024-02-29 12:21:20 +0000

    Auto-save-commit-29/02/2024--17:51:19

--- banking/angularapp/src/app/components/navbar/navbar.component.ts
+++ banking/angularapp/src/app/components/navbar/navbar.component.ts
@@ -7,16 +7,54 @@ import { AuthService } from '../../services/auth.service';
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
+// export class NavbarComponent implements OnInit {
+//   isLoggedIn: boolean = false;
+//   isAdmin: boolean = false;
+//   isCustomer: boolean = false;
+
+//   constructor(private authService: AuthService, private router: Router) {
+//     this.authService.isAuthenticated$.subscribe((authenticated) => {
+//       this.isLoggedIn = authenticated;
+//       if (this.isLoggedIn) {
+//         this.authService.getUserRole().subscribe((role) => {
+//           this.isAdmin = this.authService.isAdmin();
+//           this.isCustomer = this.authService.isCustomer();
+//         });
+//       } else {
+//         this.isAdmin = false;
+//         this.isCustomer = false;
+//       }
+//     });
+//   }
+
+//   ngOnInit(): void {
+//     this.isLoggedIn = this.authService.isAuthenticated();
+//     if (this.isLoggedIn) {
+//       this.authService.getUserRole().subscribe((role) => {
+//         this.isAdmin = this.authService.isAdmin();
+//         this.isCustomer = this.authService.isCustomer();
+//       });
+//     }
+//   }
+
+//   logout(): void {
+//     this.isLoggedIn = false;
+//     this.isAdmin = false;
+//     this.isCustomer = false;
+//     this.authService.logout();
+//     this.router.navigate(['login']);
+//   }
+// }
 export class NavbarComponent implements OnInit {
-  isLoggedIn: boolean = false;
-  isAdmin: boolean = false;
-  isCustomer: boolean = false;
+  isLoggedIn: boolean = true;
+  isAdmin: boolean = true;
+  isCustomer: boolean = true;
 
   constructor(private authService: AuthService, private router: Router) {
     this.authService.isAuthenticated$.subscribe((authenticated) => {
       this.isLoggedIn = authenticated;
       if (this.isLoggedIn) {
-        this.authService.getUserRole().subscribe((role) => {
+        this.authService.getUserRole().subscribe(() => {
           this.isAdmin = this.authService.isAdmin();
           this.isCustomer = this.authService.isCustomer();
         });
@@ -27,11 +65,12 @@ export class NavbarComponent implements OnInit {
     });
   }
 
+
   ngOnInit(): void {
     this.isLoggedIn = this.authService.isAuthenticated();
     if (this.isLoggedIn) {
-      this.authService.getUserRole().subscribe((role) => {
-        this.isAdmin = this.authService.isAdmin();
+      this.authService.getUserRole().subscribe(() => {
+        this.isAdmin = this.authService.isAdmin(); // <-- Correct way to call the isAdmin method
         this.isCustomer = this.authService.isCustomer();
       });
     }
