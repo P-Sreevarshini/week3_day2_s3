9a6ff3bf568a2c7cf3ee35d03cd3ca9336516b20
neojarvis <techadmin@iamneo.ai>
2024-03-06 08:35:15 +0000

    Auto-save-commit-06/03/2024--14:05:12

--- banking/angularapp/src/app/services/account.service.ts
+++ banking/angularapp/src/app/services/account.service.ts
@@ -8,7 +8,7 @@ import { catchError } from 'rxjs/operators';
   providedIn: 'root'
 })
 export class AccountService {
-  private apiUrl = 'api/account';
+  private apiUrl = '/api/account';
 
   constructor(private http: HttpClient) { }
 
