7dda8d4ee8b7cc9a4772526421f81bee7403dffb
neojarvis <techadmin@iamneo.ai>
2024-03-07 08:27:52 +0000

    Auto-save-commit-07/03/2024--13:57:49

--- banking/angularapp/src/app/models/fixedDepositAccount.ts
+++ banking/angularapp/src/app/models/fixedDepositAccount.ts
@@ -1,12 +1,12 @@
-import { FixedDeposit } from "./fixedDeposit.model";
-import { User } from "./user.model";
+    import { FixedDeposit } from "./fixedDeposit.model";
+    import { User } from "./user.model";
 
-export class FDAccount {
-    FDAccountId: number;
-    UserId: number;
-    Status: string;
-    FixedDepositId: number;
-    User?: User; 
-    FixedDeposit?: FixedDeposit; 
-  }
-    
+    export class FDAccount {
+        FDAccountId: number;
+        UserId: number;
+        Status: string;
+        FixedDepositId: number;
+        User?: User; 
+        FixedDeposit?: FixedDeposit; 
+    }
+        
--- edadeeddfbdaedbcaafcbfcecefbcbacdebdeecab-main/angularapp/src/app/components/add-payment/add-payment.component.ts
+++ edadeeddfbdaedbcaafcbfcecefbcbacdebdeecab-main/angularapp/src/app/components/add-payment/add-payment.component.ts
@@ -36,17 +36,6 @@ export class AddPaymentComponent implements OnInit {
       this.paymentData.courseId = params['courseID'];
       this.paymentData.course = { ...params }; // Spread the query parameters into the course object
       console.log('Payment Data:', this.paymentData);
-      // const role = localStorage.getItem('userId')
-      // console.log(role);
-      // {
-      //   "paymentID": 0,
-      //   "courseId": 0,
-      //   "userId": 0,
-      //   "status": "string",
-      //   "totalAmount": 0,
-      //   "paymentDate": "2024-02-27T06:38:42.799Z",
-      //   "modeOfPayment": "string"
-      // }
     });
   }
 
