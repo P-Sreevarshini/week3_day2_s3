3d886da40a4d922f36a733c0798bc6a90f66738c
neojarvis <techadmin@iamneo.ai>
2024-03-07 08:12:52 +0000

    Auto-save-commit-07/03/2024--13:42:49

---
+++ banking/angularapp/src/app/models/fixedDepositAccount.ts
@@ -0,0 +1,12 @@
+import { FixedDeposit } from "./fixedDeposit.model";
+import { User } from "./user.model";
+
+export class FDAccount {
+    FDAccountId: number;
+    UserId: number;
+    Status: string;
+    FixedDepositId: number;
+    User?: User; 
+    FixedDeposit?: FixedDeposit; 
+  }
+    
