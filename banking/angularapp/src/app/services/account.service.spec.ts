0e4bc50468f940446f8e5461e3669e8f8158d20e
neojarvis <techadmin@iamneo.ai>
2024-03-08 14:55:45 +0000

    Auto-save-commit-08/03/2024--20:25:42

--- banking/angularapp/src/app/services/account.service.spec.ts
+++ banking/angularapp/src/app/services/account.service.spec.ts
@@ -1,16 +1,37 @@
 import { TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { AccountService } from './account.service';
 
 describe('AccountService', () => {
   let service: AccountService;
+  let httpMock: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [AccountService],
+    });
+
     service = TestBed.inject(AccountService);
+    httpMock = TestBed.inject(HttpTestingController);
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  afterEach(() => {
+    httpMock.verify();
+  });
+
+  fit('Frontend_should get all accounts by admin', () => {
+    const mockToken = 'mockToken';
+
+    // Mock the localStorage getItem method to return the mock token
+    spyOn(localStorage, 'getItem').and.returnValue(mockToken);
+
+    service.getAllAccounts().subscribe((accounts) => {
+      expect(accounts).toBeTruthy();
+    });
+
+    const req = httpMock.expectOne(`${service.apiUrl}/api/account`);
+    expect(req.request.method).toBe('GET');
+    expect(req.request.headers.get('Authorization')).toBe('Bearer ' + mockToken);
   });
 });
--- banking/angularapp/src/app/services/fixed-deposit.service.spec.ts
+++ banking/angularapp/src/app/services/fixed-deposit.service.spec.ts
@@ -1,16 +1,44 @@
 import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 
 import { FixedDepositService } from './fixed-deposit.service';
+import { FixedDeposit } from '../models/fixedDeposit.model';
 
 describe('FixedDepositService', () => {
   let service: FixedDepositService;
+  let httpMock: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [FixedDepositService],
+    });
+
     service = TestBed.inject(FixedDepositService);
+    httpMock = TestBed.inject(HttpTestingController);
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  afterEach(() => {
+    httpMock.verify();
+  });
+
+  fit('Frontend_should save fixed deposit by admin', () => {
+    const mockFdData: FixedDeposit = {
+      FixedDepositId: 1,
+      amount: 10000,
+      tenureMonths: 12,
+      interestRate: 6,
+    };
+
+    const mockToken = 'mockToken';
+
+    (service as any).saveFdByAdmin(mockFdData, mockToken).subscribe();
+
+    const req = httpMock.expectOne(`${(service as any).apiUrl}/api/fixeddeposit`);
+    expect(req.request.method).toBe('POST');
+    expect(req.request.headers.get('Authorization')).toBe('Bearer ' + mockToken);
+
+    // provide a mock response
+    req.flush(mockFdData);
   });
 });
