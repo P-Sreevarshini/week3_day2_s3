d7506a66aa300b73bb1a7577d8cda9c75d14c2b7
neojarvis <techadmin@iamneo.ai>
2024-03-09 05:57:15 +0000

    Auto-save-commit-09/03/2024--11:27:13

--- banking/TestProject/UnitTest1.cs
+++ banking/TestProject/UnitTest1.cs
@@ -91,6 +91,32 @@ public class ApplicationTests
         Console.WriteLine(responseString);
         Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
     }
+    [Test]
+    public async Task Backend_TestLoginUser()
+    {
+        string uniqueId = Guid.NewGuid().ToString();
+
+        // Generate a unique userName based on a timestamp
+        string uniqueUsername = $"abcd_{uniqueId}";
+        string uniqueEmail = $"abcd{uniqueId}@gmail.com";
+
+        string requestBody = $"{{\"Username\": \"{uniqueUsername}\", \"Password\": \"abc@123A\", \"Email\": \"{uniqueEmail}\", \"MobileNumber\": \"1234567890\", \"UserRole\": \"Customer\"}}";
+        HttpResponseMessage response = await _httpClient.PostAsync("/api/register", new StringContent(requestBody, Encoding.UTF8, "application/json"));
+
+        // Print registration response
+        string registerResponseBody = await response.Content.ReadAsStringAsync();
+        Console.WriteLine("Registration Response: " + registerResponseBody);
+
+        // Login with the registered user
+        string loginRequestBody = $"{{\"Email\" : \"{uniqueEmail}\",\"Password\" : \"abc@123A\"}}"; // Updated variable names
+        HttpResponseMessage loginResponse = await _httpClient.PostAsync("/api/login", new StringContent(loginRequestBody, Encoding.UTF8, "application/json"));
+
+        // Print login response
+        string loginResponseBody = await loginResponse.Content.ReadAsStringAsync();
+        Console.WriteLine("Login Response: " + loginResponseBody);
+
+        Assert.AreEqual(HttpStatusCode.OK, loginResponse.StatusCode);
+    }
 
 [Test]
 public async Task Backend_Test_Post_FixedDepositByAdmin() //add fixed deposit by admin 
