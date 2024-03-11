8342d8372fe192ba3bab560a5f8dc640b96ced63
neojarvis <techadmin@iamneo.ai>
2024-03-11 18:18:39 +0000

    Auto-save-commit-11/03/2024--23:48:35

--- Console/TestProject/UnitTest1.cs
+++ Console/TestProject/UnitTest1.cs
@@ -1,5 +1,6 @@
 using NUnit.Framework;
 using System;
+using System.Collections.Generic;
 using dotnetapp;
 
 namespace TestProject
@@ -11,10 +12,28 @@ namespace TestProject
         private const string DepartmentConnectionString = "User ID=sa;password=examlyMssql@123; server=localhost;Database=DepartmentDB;trusted_connection=false;Persist Security Info=False;Encrypt=False;";
 
         [Test]
+        public void Test_InsertEmployee_Success()
+        {
+            // Arrange
+            const int empId = 4;
+            const string empName = "Alice Cooper";
+            const string email = "alice@example.com";
+            const string phoneNumber = "1234567890";
+            const string department = "Marketing";
+
+            // Act
+            Program.InsertEmployee(EmployeeConnectionString, empId, empName, email, phoneNumber, department);
+
+            // Assert
+            bool recordExists = RecordExists(EmployeeConnectionString, "Employee", empId);
+            Assert.IsTrue(recordExists, "Inserted record should exist in the database");
+        }
+
+        [Test]
         public void Test_RetrieveEmployeeData()
         {
             // Act
-            var employees = RetrieveEmployees(EmployeeConnectionString);
+            var employees = Program.RetrieveEmployees(EmployeeConnectionString);
 
             // Assert
             Assert.IsNotNull(employees, "Retrieved employee data should not be null");
@@ -25,7 +44,7 @@ namespace TestProject
         public void Test_RetrieveDepartmentData()
         {
             // Act
-            var departments = RetrieveDepartments(DepartmentConnectionString);
+            var departments = Program.RetrieveDepartments(DepartmentConnectionString);
 
             // Assert
             Assert.IsNotNull(departments, "Retrieved department data should not be null");
@@ -39,86 +58,11 @@ namespace TestProject
             const int empIdToDelete = 1; // Assuming employee with ID 1 exists
 
             // Act
-            DeleteEmployee(EmployeeConnectionString, empIdToDelete);
+            Program.DeleteEmployee(EmployeeConnectionString, empIdToDelete);
 
             // Assert
             bool recordExists = RecordExists(EmployeeConnectionString, "Employee", empIdToDelete);
             Assert.IsFalse(recordExists, "Deleted record should not exist in the database");
-        }
-        private List<Employee> RetrieveEmployees(string connectionString)
-        {
-            List<Employee> employees = new List<Employee>();
-            
-            using (SqlConnection connection = new SqlConnection(connectionString))
-            {
-                connection.Open();
-
-                string query = "SELECT * FROM Employee";
-
-                SqlCommand command = new SqlCommand(query, connection);
-                SqlDataReader reader = command.ExecuteReader();
-
-                while (reader.Read())
-                {
-                    Employee employee = new Employee
-                    {
-                        EmpId = reader.GetInt32(0),
-                        EmpName = reader.GetString(1),
-                        Email = reader.GetString(2),
-                        PhoneNumber = reader.GetString(3),
-                        Department = reader.GetString(4)
-                    };
-
-                    employees.Add(employee);
-                }
-            }
-
-            return employees;
-        }
-
-        private List<Department> RetrieveDepartments(string connectionString)
-        {
-            List<Department> departments = new List<Department>();
-            
-            using (SqlConnection connection = new SqlConnection(connectionString))
-            {
-                connection.Open();
-
-                string query = "SELECT * FROM Department";
-
-                SqlCommand command = new SqlCommand(query, connection);
-                SqlDataReader reader = command.ExecuteReader();
-
-                while (reader.Read())
-                {
-                    Department department = new Department
-                    {
-                        DeptId = reader.GetInt32(0),
-                        DeptName = reader.GetString(1),
-                        Location = reader.GetString(2),
-                        EmployeeCount = reader.GetInt32(3)
-                    };
-
-                    departments.Add(department);
-                }
-            }
-
-            return departments;
-        }
-
-        private void DeleteEmployee(string connectionString, int employeeId)
-        {
-            using (SqlConnection connection = new SqlConnection(connectionString))
-            {
-                connection.Open();
-
-                string deleteQuery = $"DELETE FROM Employee WHERE EmpId = {employeeId}";
-
-                SqlCommand command = new SqlCommand(deleteQuery, connection);
-                int rowsAffected = command.ExecuteNonQuery();
-
-                Console.WriteLine($"Deleted {rowsAffected} employee(s).");
-            }
         }
     }
 }
