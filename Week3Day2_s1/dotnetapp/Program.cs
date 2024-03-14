1e48cbc2791e2f4c888e04149e02024951ea50ab
neojarvis <techadmin@iamneo.ai>
2024-03-14 07:27:13 +0000

    Auto-save-commit-14/03/2024--12:57:09

--- Week3Day2_s1/dotnetapp/Program.cs
+++ Week3Day2_s1/dotnetapp/Program.cs
@@ -22,6 +22,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Menu}/{action=Home}/{id?}");
 
 app.Run();
