c3e4b00e5241f1d34b2c8067a4f7e220ac8386b1
neojarvis <techadmin@iamneo.ai>
2024-03-15 05:49:03 +0000

    Auto-save-commit-15/03/2024--11:18:59

--- Week4_Day1_s3_Project/dotnetapp/Program.cs
+++ Week4_Day1_s3_Project/dotnetapp/Program.cs
@@ -1,10 +1,3 @@
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using System;
-using System.Text;
-using System.Threading.Tasks;
-using dotnetapp.Models;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -13,9 +6,6 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DevConnection")));
-
 
 var app = builder.Build();
 
