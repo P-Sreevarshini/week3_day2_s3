b0aef5c0ed97d3d982f854e74ef1cda20e0ea6c4
neojarvis <techadmin@iamneo.ai>
2024-03-15 16:28:36 +0000

    Auto-save-commit-15/03/2024--21:58:32

--- Week4_Day1_s2/dotnetapp/Controllers/OrderController.cs
+++ Week4_Day1_s2/dotnetapp/Controllers/OrderController.cs
@@ -15,47 +15,47 @@ namespace dotnetapp.Controllers
             _orderService = orderService;
         }
 
-        [HttpGet]
-        public IActionResult GetAllOrders()
-        {
-            var orders = _orderService.GetAllOrders();
-            return Ok(orders);
-        }
+        // [HttpGet]
+        // public IActionResult GetAllOrders()
+        // {
+        //     var orders = _orderService.GetAllOrders();
+        //     return Ok(orders);
+        // }
 
-        [HttpGet("{id}")]
-        public IActionResult GetOrderById(int id)
-        {
-            var order = _orderService.GetOrderById(id);
-            if (order == null)
-            {
-                return NotFound();
-            }
-            return Ok(order);
-        }
+        // [HttpGet("{id}")]
+        // public IActionResult GetOrderById(int id)
+        // {
+        //     var order = _orderService.GetOrderById(id);
+        //     if (order == null)
+        //     {
+        //         return NotFound();
+        //     }
+        //     return Ok(order);
+        // }
 
-        [HttpPost]
-        public IActionResult AddOrder(Order order)
-        {
-            _orderService.AddOrder(order);
-            return CreatedAtAction(nameof(GetOrderById), new { id = order.OrderId }, order);
-        }
+        // [HttpPost]
+        // public IActionResult AddOrder(Order order)
+        // {
+        //     _orderService.AddOrder(order);
+        //     return CreatedAtAction(nameof(GetOrderById), new { id = order.OrderId }, order);
+        // }
 
-        [HttpPut("{id}")]
-        public IActionResult UpdateOrder(int id, Order order)
-        {
-            if (id != order.OrderId)
-            {
-                return BadRequest();
-            }
-            _orderService.UpdateOrder(order);
-            return NoContent();
-        }
+        // [HttpPut("{id}")]
+        // public IActionResult UpdateOrder(int id, Order order)
+        // {
+        //     if (id != order.OrderId)
+        //     {
+        //         return BadRequest();
+        //     }
+        //     _orderService.UpdateOrder(order);
+        //     return NoContent();
+        // }
 
-        [HttpDelete("{id}")]
-        public IActionResult DeleteOrder(int id)
-        {
-            _orderService.DeleteOrder(id);
-            return NoContent();
-        }
+        // [HttpDelete("{id}")]
+        // public IActionResult DeleteOrder(int id)
+        // {
+        //     _orderService.DeleteOrder(id);
+        //     return NoContent();
+        // }
     }
 }
