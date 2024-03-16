c39ef598484f13634f87ae049dcca82d05f65e09
neojarvis <techadmin@iamneo.ai>
2024-03-16 14:20:07 +0000

    Auto-save-commit-16/03/2024--19:50:03

--- week5_day2_s1/angularapp/src/app/components/authguard/auth.guard.ts
+++ week5_day2_s1/angularapp/src/app/components/authguard/auth.guard.ts
@@ -3,7 +3,6 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, Router } from '@angular/router';
 import { AuthService } from 'src/app/services/auth.service';
-import { tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -12,12 +11,11 @@ export class AuthGuard implements CanActivate {
   constructor(private authService: AuthService, private router: Router) {}
 
   canActivate(): boolean {
-    return this.authService.isAuthenticated().pipe(
-      tap(isAuthenticated => {
-        if (!isAuthenticated) {
-          this.router.navigate(['/login']); // User is not authenticated, redirect to the login page
-        }
-      })
-    );
+    if (this.authService.login()) {
+      return true;
+    } else {
+      this.router.navigate(['/login']);
+      return false;
+    }
   }
 }
