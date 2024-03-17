772888ddc43a6b680074ef80d57c9e90468de3db
neojarvis <techadmin@iamneo.ai>
2024-03-17 16:48:47 +0000

    Auto-save-commit-17/03/2024--22:18:44

---
+++ week5_day2_s3/dotnetapp/Controllers/AuthController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using dotnetapp.Models; // Add this line to import the namespace
+
+// [Route("api/[controller]")]
+[ApiController]
+public class AuthController : ControllerBase
+{
+    [HttpPost("api/login")]
+    public IActionResult Login([FromBody] LoginModel login)
+    {
+        if (login.Username == "admin" && login.Password == "password")
+        {
+            return Ok(new { message = "Login successful" });
+        }
+        else
+        {
+            return Unauthorized(new { message = "Invalid username or password" });
+        }
+    }
+}
+
