23d57044ed99184a97397834084468bf4a52009e
neojarvis <techadmin@iamneo.ai>
2024-03-18 04:54:30 +0000

    Auto-save-commit-18/03/2024--10:24:26

--- week5_day2_s1/angularapp/src/app/components/authguard/auth.guard.ts
+++ week5_day2_s1/angularapp/src/app/components/authguard/auth.guard.ts
@@ -11,7 +11,7 @@ export class AuthGuard implements CanActivate {
   constructor(private authService: AuthService, private router: Router) {}
 
   canActivate(): boolean {
-    if (this.authService.login()) {
+    if (this.authService.login(username, password)) {
       return true;
     } else {
       this.router.navigate(['/login']);
