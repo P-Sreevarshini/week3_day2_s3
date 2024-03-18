ffcc999dcbb88e220d42328deea2b3bf5081bc4e
neojarvis <techadmin@iamneo.ai>
2024-03-18 06:24:31 +0000

    Auto-save-commit-18/03/2024--11:54:27

--- week5_day2_s2/angularapp/src/app/services/jwt.service.spec.ts
+++ week5_day2_s2/angularapp/src/app/services/jwt.service.spec.ts
@@ -1,16 +1,43 @@
-import { TestBed } from '@angular/core/testing';
-
-import { JwtService } from './jwt.service';
+import { JwtService } from "./jwt.service";
 
 describe('JwtService', () => {
   let service: JwtService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(JwtService);
+    service = new JwtService();
   });
 
-  it('should be created', () => {
+  fit('JwtService_should_be_created', () => {
     expect(service).toBeTruthy();
   });
+
+  fit('JwtService_should_save_token_to_local_storage', () => {
+    const token = 'sample_token';
+    service.saveToken(token);
+    expect(localStorage.getItem('jwtToken')).toEqual(token);
+  });
+
+  fit('JwtService_should_get_token_from_local_storage', () => {
+    const token = 'sample_token';
+    localStorage.setItem('jwtToken', token);
+    expect(service.getToken()).toEqual(token);
+  });
+
+  fit('JwtService_should_remove _token_from_local_storage', () => {
+    const token = 'sample_token';
+    localStorage.setItem('jwtToken', token);
+    service.destroyToken();
+    expect(localStorage.getItem('jwtToken')).toBeNull();
+  });
+
+  fit('JwtService_should_return_true_if_user_is_loggedIn', () => {
+    const token = 'sample_token';
+    localStorage.setItem('jwtToken', token);
+    expect(service.isLoggedIn()).toBeTrue();
+  });
+
+  // it('should return false if user is not logged in', () => {
+  //   localStorage.removeItem('jwtToken');
+  //   expect(service.isLoggedIn()).toBeFalse();
+  // });
 });
