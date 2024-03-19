0ef480351fb2e22579bb9c0f06e743ae9fe41524
neojarvis <techadmin@iamneo.ai>
2024-03-19 08:42:42 +0000

    Auto-save-commit-19/03/2024--14:12:38

--- Week3_day2/s3/dotnetapp/Migrations/20240319083825_initial.Designer.cs
+++ Week3_day2/s3/dotnetapp/Migrations/20240319083825_initial.Designer.cs
@@ -12,7 +12,7 @@ using dotnetapp.Data;
 namespace dotnetapp.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    [Migration("20240319044611_initial")]
+    [Migration("20240319083825_initial")]
     partial class initial
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
--- Week3_day2/s3/dotnetapp/Program.cs
+++ Week3_day2/s3/dotnetapp/Program.cs
@@ -12,8 +12,13 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
 
 var app = builder.Build();
 
+
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage(); // Add this line
+}
+else
 {
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
